8122ad87203cb5d1014a468a068a7c0078ab4fb4
Iavosoa RATOANDROMANANA <miella.ratoandromanana@etu.univ-lyon1.fr>
2023-11-22 15:10:11 +0100

    partie 2 implémentation pièces côté modèle

---
+++ src/src/Modele/ZTetromino.java
@@ -0,0 +1,27 @@
+package Modele;
+
+public class ZTetromino extends Pieces{
+    /* clé pour identifier dans le dictionnaire */
+
+    String cle = "Z";
+
+    /* matrice initiale */
+
+    public final int[][] matrxZ = {
+            {0, 0, 0},
+            {1, 1, 0},
+            {0, 1, 1}
+    };
+
+    /* matrice reverse */
+
+    public final int [][] matrxZr = {
+            {1, 0, 0},
+            {1, 1, 0},
+            {0, 1, 0}
+    };
+
+    public ZTetromino() {
+        super.pieces.put(cle, matrxZ);
+    }
+}
